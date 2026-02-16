74957bb82babc595abcad366c44358c5d359c35d
Tawkie <tawkie@agentmail.to>
2026-02-16 11:06:28 -0800

    Add provider architecture diagram with @arach/arc

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Posterous from './pages/Posterous'
 import { GettingStarted } from './pages/GettingStarted'
 import { API } from './pages/API'
 import { Workflows } from './pages/Workflows'
+import { ProviderDiagram } from './pages/ProviderDiagram'
 
 function Nav() {
   const location = useLocation()
@@ -17,6 +18,7 @@ function Nav() {
         <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link></li>
         <li><Link to="/posterous" className={location.pathname === '/posterous' ? 'active' : ''}>Posterous</Link></li>
         <li><Link to="/docs/getting-started" className={location.pathname.startsWith('/docs') ? 'active' : ''}>Docs</Link></li>
+        <li><Link to="/providers" className={location.pathname === '/providers' ? 'active' : ''}>Providers</Link></li>
         <li><a href="https://github.com/tawkiebot/clawderous">GitHub</a></li>
       </ul>
       <a href="https://github.com/tawkiebot/clawderous" className="nav-cta">Get Started</a>
@@ -43,6 +45,7 @@ export default function App() {
         <Route path="/docs/getting-started" element={<GettingStarted />} />
         <Route path="/docs/api" element={<API />} />
         <Route path="/docs/workflows" element={<Workflows />} />
+        <Route path="/providers" element={<ProviderDiagram />} />
       </Routes>
       <Footer />
     </BrowserRouter>
